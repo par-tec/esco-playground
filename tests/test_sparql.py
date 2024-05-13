3c87c71356260342dc39a3516f7890c88c878446
Manuel Audisio <audisio54@gmail.com>
2024-05-13 12:24:56 +0200

    test C25 escoSkill

--- tests/test_sparql.py
+++ tests/test_sparql.py
@@ -1,3 +1,6 @@
+import io
+
+import pandas as pd
 import pytest
 
 import esco.sparql
@@ -28,3 +31,45 @@ def test_can_load_occupations_from_sparql(sparql):
     assert sparql
     ret = sparql.load_occupations()
     assert len(ret) > 70
+
+
+def test_if_skills_exists_in_json(sparql):
+    assert sparql
+    ret = sparql.load_skills()
+    categories = categories or [  # Defaults to ICT professionals and technicians.
+        "http://data.europa.eu/esco/isco/C25",
+        "http://data.europa.eu/esco/isco/C35",
+    ]
+
+    categories = "\n".join([f"<{uri}>" for uri in categories])
+
+    res = sparql.query(
+        """
+
+    SELECT DISTINCT ?s ?label WHERE {
+
+    VALUES ?category { """
+        + categories
+        + """ }
+
+    ?uri a esco:Occupation ;
+        (esco:relatedEssentialSkill | esco:relatedOptionalSkill) ?s ;
+        skos:broaderTransitive* ?category  ;
+        iso-thes:status "released"
+    .
+
+    # Get current skill labels associated
+    #  with the occupation.
+    ?s iso-thes:status "released" .
+
+
+
+    FILTER (lang(?label) = "en")
+                    }"""
+    )
+    df = pd.read_csv(io.StringIO(res.decode()))
+
+    for skill in df["s"]:
+        assert skill in ret.uri
+
+    raise NotImplementedError
