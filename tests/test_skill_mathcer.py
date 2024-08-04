00605cde10f4c9d4779fe5f8b023f27cd2df6458
Roberto Polli <robipolli@gmail.com>
2024-08-05 00:27:30 +0200

    Preliminary reorganization.

--- tests/test_skill_mathcer.py
+++ tests/test_skill_mathcer.py
@@ -1,102 +1,100 @@
+import pytest
 import spacy
 from spacy.matcher import Matcher
 
-nlp = spacy.load("en_core_web_trf")
-matcher = Matcher(nlp.vocab)
-matched_sents = []
 
-"""
-'assure customer satisfaction',
-'customer satisfaction guarantee',
-'ensure customer satisfaction',
-'guarantee customer satisfaction',
-'guaranteeing customer satisfaction',
-'promise customer satisfaction',
-'provide customer satisfaction',
-'to guarantee customer satisfaction']
-"""
+@pytest.fixture
+def nlp():
+    return spacy.load("en_core_web_trf")
 
-pattern = [
-    {"LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]}},
-    {"POS": "DET", "OP": "?"},
-    {"TEXT": "customer", "OP": "?"},
-    {"TEXT": "satisfaction"},
-]
-pattern1 = [
-    {"TEXT": "customer"},
-    {"TEXT": "satisfaction"},
-    {
-        "LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]},
-        "OP": "?",
-    },
-]
-pattern2 = [
-    {
-        "LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]},
-        "OP": "?",
-    },
-    {"POS": "DET", "OP": "?"},
-    {"TEXT": "customer", "OP": "?"},
-    {"TEXT": "satisfaction"},
-    {
-        "LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]},
-        "OP": "?",
-    },
-]
-matcher.add("MatcherSkill", [pattern2])  # add pattern
-doc = nlp("I have a long experience in guaranteeing customer satisfaction ")
-doc1 = nlp("I have a long experience in providing satisfaction ")
-doc2 = nlp(
-    "I have a long experience in assure customer satisfaction and promise the customer satisfaction "
-)
-matches = matcher(doc)
-matches1 = matcher(doc1)
-matches2 = matcher(doc2)
-print("*************test_1*************")
-for match_id, start, end in matches:
-    matched_span = doc[start:end]
-    print(matched_span)
-print("*************test_2*************")
-for match_id, start, end in matches1:
-    matched_span = doc1[start:end]
-    print(matched_span)
-print("*************test_3*************")
-for match_id, start, end in matches2:
-    matched_span = doc2[start:end]
-    print(matched_span)
 
-# cloud position
-
-matched_sents = []
-
-pattern3 = [
-    {
-        "LEMMA": {
-            "IN": [
-                "design",
-                "deploy",
-                "plan",
-                "develop",
-                "manage",
-                "automate",
-                "implement",
-                "migrate",
-            ]
+CUSTOMER_SATISFACTION_PATTERNS = dict(
+    pattern=[
+        {"LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]}},
+        {"POS": "DET", "OP": "?"},
+        {"TEXT": "customer", "OP": "?"},
+        {"TEXT": "satisfaction"},
+    ],
+    pattern1=[
+        {"TEXT": "customer"},
+        {"TEXT": "satisfaction"},
+        {
+            "LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]},
+            "OP": "?",
         },
-        "OP": "+",
-    },
-    {"POS": "ADJ", "OP": "?"},
-    {"POS": "NOUN", "OP": "?"},
-    {"POS": "DET", "OP": "?"},
-    {"LOWER": "cloud"},
-    {"POS": "NOUN", "OP": "?"},
-]
-matcher.add("CloudSkill", [pattern3])
-doc = nlp(
-    "manage and maintain company IT infrastructure implement hybrid cloud solutions and design cloud architecture and plan migration cloud optimize"
+    ],
+    pattern2=[
+        {
+            "LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]},
+            "OP": "?",
+        },
+        {"POS": "DET", "OP": "?"},
+        {"TEXT": "customer", "OP": "?"},
+        {"TEXT": "satisfaction"},
+        {
+            "LEMMA": {"IN": ["guarantee", "assure", "ensure", "provide", "promise"]},
+            "OP": "?",
+        },
+    ],
 )
-matches = matcher(doc)
 
-for match_id, start, end in matches:
-    matched_span = doc[start:end]
-    print(matched_span)
+CUSTOMER_SATISFACTION_TEXTS = [
+    "I have a long experience in guaranteeing customer satisfaction ",
+    "I have a long experience in providing satisfaction ",
+    "I have a long experience in assure customer satisfaction and promise the customer satisfaction ",
+]
+
+
+@pytest.mark.parametrize("text", CUSTOMER_SATISFACTION_TEXTS)
+@pytest.mark.parametrize("pattern", CUSTOMER_SATISFACTION_PATTERNS.values())
+def test_can_match_customer_satisfaction(nlp, text, pattern):
+    doc = nlp(text)
+    matcher = Matcher(nlp.vocab)
+    matcher.add("MatcherSkill", [pattern])
+    matches = matcher(doc)
+    for match_id, start, end in matches:
+        matched_span = doc[start:end]
+        print(matched_span)
+        assert matched_span.text == " ".join(pattern)
+
+
+CLOUD_PATTERNS = dict(
+    pattern=[
+        {
+            "LEMMA": {
+                "IN": [
+                    "design",
+                    "deploy",
+                    "plan",
+                    "develop",
+                    "manage",
+                    "automate",
+                    "implement",
+                    "migrate",
+                ]
+            },
+            "OP": "+",
+        },
+        {"POS": "ADJ", "OP": "?"},
+        {"POS": "NOUN", "OP": "?"},
+        {"POS": "DET", "OP": "?"},
+        {"LOWER": "cloud"},
+        {"POS": "NOUN", "OP": "?"},
+    ]
+)
+CLOUD_TEXTS = [
+    "manage and maintain company IT infrastructure implement hybrid cloud solutions and design cloud architecture and plan migration cloud optimize",
+]
+
+
+@pytest.mark.parametrize("text", CLOUD_TEXTS)
+@pytest.mark.parametrize("pattern", CLOUD_PATTERNS.values())
+def test_can_match_cloud(nlp, text, pattern):
+    doc = nlp(text)
+    matcher = Matcher(nlp.vocab)
+    matcher.add("MatcherSkill", [pattern])
+    matches = matcher(doc)
+    for match_id, start, end in matches:
+        matched_span = doc[start:end]
+        print(matched_span)
+        assert matched_span.text == " ".join(pattern)
